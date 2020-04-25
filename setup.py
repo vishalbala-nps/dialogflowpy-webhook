b2274e92c010f57a91a3a59df0272c048d32960e
Vishal Balasubramanian <vishal.bala.test@gmail.com>
2020-04-25 13:07:41 +0530

    Renamed from dialogflowpy to dialogflowpy-webhook

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup,find_packages
 setup(
-    name="dialogflowpy",
+    name="dialogflowpy-webhook",
     version="0.0.1",
     description="A Python module for parsing and creating Dialogflow Requests and Responses",
-    url="https://github.com/vishalbala-nps/dialogflowpy",
-    author="vishalbala-nps",
+    url="https://github.com/vishalbala-nps/dialogflowpy-webhook",
+    author="Vishal Balasubramanian",
     author_email="vishal.bala.test@gmail.com",
     license="MIT",
     packages=find_packages(),
