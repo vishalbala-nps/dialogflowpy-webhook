04cd85f787b00de505caa0512c919bf3245f68a2
Vishal Balasubramanian <vishal.bala.nps@gmail.com>
2020-04-26 16:36:38 +0530

    Fixed Readme bug

--- setup.py
+++ setup.py
@@ -1,7 +1,9 @@
 from setuptools import setup,find_packages
+from os import path
 
-with open("./README.md","r") as f:
-    longdesc = f.read()
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name="dialogflowpy-webhook",
@@ -10,12 +12,12 @@ setup(
     url="https://github.com/vishalbala-nps/dialogflowpy-webhook",
     author="Vishal Balasubramanian",
     author_email="vishal.bala.nps@gmail.com",
-    long_description=longdesc,
+    long_description=long_description,
     long_description_content_type="text/markdown",
     license="MIT",
     packages=find_packages(),
     python_requires=">=3",
-    keywords=["dialogflow","flask","django","fulfillment","webhook","api","python3","api.ai","python","Dialogflow Fulfillment","Dialogflow Fulfillment V2","dialogflowpy-webhook"],
+    keywords=["dialogflow","flask","django","fulfillment","webhook","api","python3","api.ai","python","Dialogflow Fulfillment","Dialogflow Fulfillment V2","dialogflowpy-webhook","artificial intelligence"],
     classifiers=[
         'Intended Audience :: Developers',
         'Topic :: Software Development :: Build Tools',
